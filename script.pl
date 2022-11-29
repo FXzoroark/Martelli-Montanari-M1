3ea58ec025c535784be2770c940c214152f75017
naomi <tkonao70@gmail.com>
2022-11-29 22:01:07 +0100

    plein de truc

--- script.pl
+++ script.pl
@@ -25,7 +25,7 @@ echo(_).
 regles(X?=T, rename) :- var(X), var(T),!.  
 
 % Simplify
-% renvoie vrai si X est une constante
+% renvoie vrai si T est une constante
 regles(X?=T, simplify) :- var(X), atomic(T),!.
 
 % Expand
@@ -54,65 +54,101 @@ occur_check(V, T) :- var(V), compound(T), contains_var(V, T),!.
 %---------------------------------------------------------------------------------------------------
 % Réduits
 
+% Renommage d'une variable
+% Renomme toute les occurence de X par la variable T dans P et met le résultat dans  Q.
 reduit(rename, X?=T, P, Q) :- X=T, Q=P.
 
+% Simplification d'une constante
+% Renomme toute les occurence de X par la constante T dans P et met le résultat dans  Q.
 reduit(simplify, X?=T, P,Q) :- X=T, Q=P.
 
+% Unification d'une variable avec un terme composé
+% Renomme les occurences de X par le terme composé T dans P et met le résultat dans Q.
 reduit(expand, X?=T, P, Q) :- X=T, Q=P.
 
+% Check
+% Fail car occur check trouvé par regles(X?=T, check).
 reduit(check, _, _, _):- echo("\n No\n"), fail.
 
-reduit(orient, T?=X, P, Q) :- append(P, [X?=T], Q).
+% Echange
+% Permute le terme T avec la variable X
+reduit(orient, T?=X, P, Q) :- append([X?=T], P, Q).
 
+% Décomposition de deux fonctions
+% Décompose deux à deux les arguments de deux termes composés.
 reduit(decompose, F?=G, P, Q) :- F =.. [_|FARGS], G =.. [_|GARGS], decomposition(FARGS, GARGS, RES), append(RES, P, Q).
 
+% Clash
+% Fail car clash trouvé par regles(F?=G, clash).
 reduit(clash, _, _, _):- echo("\n No\n"), fail.
 
+% Ajoute la relation ?= entre chaque éléments des deux listes données.
 decomposition([H1|T1], [H2|T2], RES) :- decomposition(T1, T2, ACC), append([H1?=H2], ACC, RES).
 decomposition([], [], []).
 
 %---------------------------------------------------------------------------------------------------
 
-unifie_([H|T], STRATEGY) :- aff_sys(H|T), regles(H, R), aff_regle(R, H), reduit(R, H, T, Q), unifie(Q, STRATEGY).
-unifie_([], _) :- echo("\n Yes\n").
+unifie_([H|T], S) :- aff_sys(H|T), regles(H, R), aff_regle(R, H), reduit(R, H, T, Q), unifie(Q, S).
+unifie_(fin) :- echo("\n Yes\n").
 
 %---------------------------------------------------------------------------------------------------
 %   QUESTION 2.
 %---------------------------------------------------------------------------------------------------
 
-
+% Associe un poids à une équation en fonction de la première règle applicable à cette dernière.
 get_i(E, I) :- (regles(E, clash); regles(E, check)) -> I = 5, !;
                (regles(E, rename); regles(E, simplify)) -> I = 4, !;
                regles(E, orient) -> I = 3, !;
                regles(E, decompose) -> I = 2, !;
                regles(E, expand) -> I = 1, !.
 
+% Compare deux à deux le poids associé aux équations afin d'obtenir celle de poids le plus élévé.
 choix_eq([H|T], TMP, E, RES) :- get_i(E, I1), get_i(H, I2),
                                 (I1>=I2 -> append([H], TMP, NEWTMP),choix_eq(T, NEWTMP, E, RES);
                                     (choix_eq(T, [E|TMP], H, RES))
                                 ).
-choix_eq([], TMP, E, RES) :- append([E], TMP , RES). %,!.
+choix_eq([], TMP, E, RES) :- append([E], TMP , RES).
 
-
+% Prend le dernier élément de la liste et le met au début.
 last_list(P, [LAST|RESTE]) :- reverse(P, [LAST|R]), reverse(R, RESTE).
 
 %---------------------------------------------------------------------------------------------------
+% Unification avec les différentes stratégies.
 
-unifie(P, choix_premier) :- unifie_(P, choix_premier).
 
-unifie([H|T], choix_pondere_1) :- choix_eq(T, [], H, Q), unifie_(Q, choix_pondere_1).
+unifie(P, choix_premier) :- echo(P),unifie_(P, choix_premier).
 
-unifie(P, choix_dernier) :- last_list(P, RES), unifie_(RES, choix_dernier).
+unifie([H|T], choix_pondere) :- echo([H|T]),choix_eq(T, [], H, Q), unifie_(Q, choix_pondere).
+
+unifie(P, choix_dernier) :- echo(P),last_list(P, RES), unifie_(RES, choix_dernier).
+
+unifie([], _) :- unifie_(fin).
 
 %---------------------------------------------------------------------------------------------------
 %   QUESTION 3.
 %---------------------------------------------------------------------------------------------------
 
+% Désactive l'affichage de la trace.
 unif(P,S) :- clr_echo, unifie(P,S).
 
+% Active l'affichage de la trace.
 trace_unif(P,S) :- set_echo, unifie(P,S).
 
 %---------------------------------------------------------------------------------------------------
-% Predicat pour l'affichage
+% Prédicat pour l'affichage
 aff_sys(P) :- echo('system: '),echo(P),echo('\n').
 aff_regle(R,E) :- echo(R),echo(': '),echo(E),echo('\n').
+
+
+
+:- initialization(main).
+
+main :- set_echo,
+        write("\n\nAlgorithme d\'unification de Martelli-Montanari\n\n"),
+        write("P est un système d'équation du type [X1?=Y1, ... ,Xn?=Yn] où Xi, Yi peuvent être des termes (f(X)), des variables (X) ou des constantes (a).\n\n"),
+        write("S est la stratégie d'unification souhaitée:\nchoix_premier\nchoix_pondere\nchoix_dernier\n\n"),
+        write("Par défaut l'affichage des étapes est activé.\n\n")    ,
+        write("Pour désactiver l'affichage des étapes et unifier, saisir:\ntrace_unif(P,S).\n\n"),
+        write("Pour activer l'affichage des étapes et unifier, saisir:\nunif(P,S).\n\n"),
+        write("Pour simplement unifier, saisir:\nunifie(P,S).\n\n\n").
+
--- test.pl
+++ test.pl
@@ -2,7 +2,101 @@
 include(script).
 
 %test(oui, [forall(member(R ,[check, clash]))]) :- regles(f(X) ?= g(X), R).
-test(oui, [fail]) :- regles(f(X) ?= X, clash).
-test(non) :- regles(f(X) ?= X, clash).
+test(rename) :- regles(X?=T, rename).
+test(rename, [fail]) :- regles(a?=b, rename).
+test(rename, [fail]) :- regles(X?=a, rename).
+test(rename, [fail]) :- regles(f(X)?=a, rename).
+
+test(simplify) :- regles(X?=a, simplify).
+test(simplify, [fail]) :- regles(a?=b, simplify).
+test(simplify, [fail]) :- regles(X?=T, simplify).
+test(simplify, [fail]) :- regles(f(X)?=a, simplify).
+
+test(expand) :- regles(X?=f(Y), expand).
+test(expand) :- regles(X?=f(a), expand).
+test(expand, [fail]) :- regles(X?=f(X), expand).
+test(expand, [fail]) :- regles(f(X)?=X, expand).
+test(expand, [fail]) :- regles(a?=f(a), expand).
+
+test(check) :- regles(X?=f(X), check).
+test(check, [fail]) :- regles(X?=f(Y), check).
+test(check, [fail]) :- regles(f(X)?=X, check).
+test(check, [fail]) :- regles(X?=X, check).
+
+test(orient) :- regles(f(X)?=X, orient).
+test(orient, [fail]) :- regles(T?=X, orient).
+test(orient, [fail]) :- regles(T?=f(X), orient).
+
+test(decompose) :- regles(f(X,Y)?=f(Y,Z), decompose).
+test(decompose, [fail]) :- regles(X?=f(Y,Z), decompose).
+test(decompose, [fail]) :- regles(f(X,Y)?=Y, decompose).
+test(decompose, [fail]) :- regles(f(X,Y)?=g(Y,Z), decompose).
+test(decompose, [fail]) :- regles(f(X,Y)?=f(Y), decompose).
+
+test(clash) :- regles(f(X,Y)?=g(Y,Z), clash).
+test(clash) :- regles(f(X,Y)?=f(Y), clash).
+test(clash, [fail]) :- regles(f(X,Y)?=f(Y,Z), clash).
+test(clash, [fail]) :- regles(X?=f(Y,Z), clash).
+test(clash, [fail]) :- regles(f(X,Y)?=Y, clash).
+
+%test(vide, [forall(member(R ,[rename, simplify, expand, check, orient, decompose, clash]))]) :- regles(, R).
 
 :- end_tests(regles).
+
+:- begin_tests(reduit).
+
+test(vide, [forall(member(R ,[rename, simplify, expand])), true(Q == [])]) :- reduit(R, X?=T, [], Q).
+
+test(rename, [true(Q == [T?=a])]) :- reduit(rename, X?=T, [X?=a], Q).
+test(rename, [true(Q == [Z?=a])]) :- reduit(rename, X?=T, [Z?=a], Q).
+test(rename, [true(Q == [T?=a])]) :- reduit(rename, X?=T, [T?=a], Q).
+test(rename, [true(Q == [Z?=T])]) :- reduit(rename, X?=T, [Z?=X], Q).
+
+test(simplify, [true(Q == [a?=T])]) :- reduit(simplify, X?=a, [X?=T], Q).
+test(simplify, [true(Q == [Y?=T])]) :- reduit(simplify, X?=a, [Y?=T], Q).
+test(simplify, [true(Q == [Y?=a])]) :- reduit(simplify, X?=a, [Y?=X], Q).
+
+test(expand, [true(Q == [f(Y)?=T])]) :- reduit(expand, X?=f(Y), [X?=T], Q).
+test(expand, [true(Q == [Y?=T])]) :- reduit(expand, X?=f(Y), [Y?=T], Q).
+test(expand, [true(Q == [Y?=f(Y)])]) :- reduit(expand, X?=f(Y), [Y?=X], Q).
+
+test(orient, [true(Q == [X?=T])]) :- reduit(orient, T?=X, [], Q).
+test(orient, [true(Q == [X?=T, X?=Y])]) :- reduit(orient, T?=X, [X?=Y], Q).
+test(orient, [true(Q == [X?=f(Y)])]) :- reduit(orient, f(Y)?=X, [], Q).
+test(orient, [true(Q == [X?=a])]) :- reduit(orient, a?=X, [], Q).
+
+test(decompose, [true(Q == [X?=Y])]) :- reduit(decompose, f(X)?=f(Y), [], Q).
+test(decompose, [true(Q == [X?=Y, X?=T])]) :- reduit(decompose, f(X)?=f(Y), [X?=T], Q).
+test(decompose, [true(Q == [X?=Y, Y?=Z])]) :- reduit(decompose, f(X,Y)?=f(Y,Z), [], Q).
+test(decompose, [true(Q == [X?=Y, Y?=a])]) :- reduit(decompose, f(X,Y)?=f(Y,a), [], Q).
+test(decompose, [true(Q == [g(X)?=Y, Y?=a])]) :- reduit(decompose, f(g(X),Y)?=f(Y,a), [], Q).
+
+:- end_tests(reduit).
+
+:- begin_tests(strategy).
+
+test(get_i, [true(I == 1)]) :- get_i(X?=f(Y), I).
+test(get_i, [true(I == 2)]) :- get_i(f(X)?=f(Y), I).
+test(get_i, [true(I == 3)]) :- get_i(f(T)?=X, I).
+test(get_i, [true(I == 4)]) :- get_i(X?=T, I).
+test(get_i, [true(I == 4)]) :- get_i(X?=a, I).
+test(get_i, [true(I == 5)]) :- get_i(X?=f(X), I).
+test(get_i, [true(I == 5)]) :- get_i(f(X)?=g(X), I).
+
+test(choix_eq, [true(RES == [X?=f(X), X?=T, f(X)?=f(Z)])]) :- choix_eq([f(X)?=f(Z), X?=f(X)], [], X?=T, RES).
+test(choix_eq, [true(RES == [f(X)?=g(Z), X?=f(X), X?=T])]) :- choix_eq([f(X)?=g(Z), X?=f(X)], [], X?=T, RES).
+test(choix_eq, [true(RES == [X?=T])]) :- choix_eq([], [], X?=T, RES).
+
+test(last_list, [true(RES == [X?=T])]) :- last_list([X?=T], RES).
+test(last_list, [true(RES == [X?=a, X?=T])]) :- last_list([X?=T, X?=a], RES).
+test(last_list, [true(RES == [f(Y) ?= Z, X?=T, X?=a])]) :- last_list([X?=T, X?=a, f(Y) ?= Z], RES).
+
+:- end_tests(strategy).
+
+:- begin_tests(unifie_).
+
+test(unifie, [forall(member(STRATEGY ,[choix_premier, choix_pondere, choix_dernier]))]) :- unifie([f(X,Y)?= f(g(Z), h(a)), Z ?= f(Y)], STRATEGY).
+test(unifie, [forall(member(STRATEGY ,[choix_premier, choix_pondere, choix_dernier])), fail]) :- unifie([f(X,Y)?= f(g(Z), h(a)), Z ?= f(Y), X ?= f(X)], STRATEGY).
+
+
+:- end_tests(unifie_).
